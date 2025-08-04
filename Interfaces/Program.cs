fb769b0df25133caa053b343aed31835aa9aab2e
Esinam01 <meahiakedzi001@st.ug.edu.gh>
2025-08-04 14:41:13 +0100

    interfaces commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Movement Demo!");
+            Console.WriteLine("Type 'car' or 'bicycle' to see it move. Type 'exit' to quit.\n");
+
+            while (true)
+            {
+                Console.Write("Enter your choice: ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                IMovable movable;
+
+                switch (input)
+                {
+                    case "car":
+                        movable = new Car();
+                        break;
+                    case "bicycle":
+                        movable = new Bicycle();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter 'car', 'bicycle', or 'exit'.\n");
+                        continue;
+                }
+
+                movable.Move();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Thanks for using the program. Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
